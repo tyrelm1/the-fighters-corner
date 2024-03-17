36e6034d02ad7c22f6d2aac614626d9b801a7ce1
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-03-17 18:02:39 +0000

    performed collectstatic to ensure summernote worked in admin and created login and logout templates and form.py

--- posts/admin.py
+++ posts/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Post)
+
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title',)  
+    list_display = ('title', 'slug', 'status')
     search_fields = ['title']
-    list_filter = ()  # Remove 'status' from list_filter
+    list_filter = ('status',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
