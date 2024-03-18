06a79e1d73006fd96308a4ce8eb6034c48f45abe
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-03-18 12:38:03 +0000

    added additional js functionality to my signup form and added an edit and delete post templates for the blog

--- static/script.js
+++ static/script.js
@@ -1,10 +1,12 @@
-//password fail criteria//
-document.getElementById("signup-form").addEventListener("submit", function(event) {
-    var password1 = document.getElementById("form_password1").value;
-    var password2 = document.getElementById("form_password2").value;
-    
-    if (password1.length < 8 || password2.length < 8) {
-        event.preventDefault(); // Prevent form submission
-        document.getElementById("password-requirements").style.display = "block"; // Show password requirements message
-    }
+//signup form submission//
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("signup-form").addEventListener("submit", function(event) {
+        var password1 = document.getElementById("id_password1").value;
+        var password2 = document.getElementById("id_password2").value;
+
+        if (password1.length < 8 || password2.length < 8) {
+            event.preventDefault(); // Prevent form submission
+            document.getElementById("password-requirements").style.display = "block"; // Show password requirements message
+        }
+    });
 });
