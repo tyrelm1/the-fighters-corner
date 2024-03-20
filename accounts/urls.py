cec6f75fc5e69feacac1153abeeb326eb2cf43ed
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-03-20 19:04:44 +0000

    added auth templates to help my signup accounts pages style correctly

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from accounts.views import signup_view
 
 urlpatterns = [
-    path('accounts/', signup_view, name='account_signup'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('signup/', signup_view, name='signup'),
+    path('accounts/signup/', signup_view, name='account_signup'),  # Custom signup view
+    path('accounts/login/', LoginView.as_view(template_name='login.html'), name='login'),  # Django login view
+    path('accounts/logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),  # Django logout view
 ]
