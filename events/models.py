ca3459ffad71a8e23449b41f9e391bc860ff924f
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-03-23 01:04:57 +0000

    adding and styling containers to enable better user accessability

--- events/models.py
+++ events/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateField()
+    featured_image = CloudinaryField('image', default='placeholder')
     location = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
 
