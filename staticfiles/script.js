5593898be347cc7baaa1275cf3b85f08e07ca511
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-03-23 21:32:16 +0000

    performed collectstatic command to ensure all changes apply to heroku deployment

--- staticfiles/script.js
+++ staticfiles/script.js
@@ -27,3 +27,53 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+// Function to display alert message for comment actions
+function showCommentAlert(action) {
+    alert('Your comment has been ' + action + ' successfully!');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Add event listener for comment edit form submission
+    var commentEditForm = document.getElementById('comment_edit_form');
+    if (commentEditForm) {
+        commentEditForm.addEventListener('submit', function() {
+            showCommentAlert('edited'); // Call showCommentAlert function when the form is submitted
+        });
+    }
+
+    // Add event listener for comment delete form submission
+    var commentDeleteForm = document.getElementById('comment_delete_form');
+    if (commentDeleteForm) {
+        commentDeleteForm.addEventListener('submit', function() {
+            showCommentAlert('deleted'); // Call showCommentAlert function when the form is submitted
+        });
+    }
+});
+
+// JavaScript for toggling navigation links
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const nav = document.querySelector('nav');
+  
+    // Toggle navigation links visibility when the hamburger button is clicked
+    navbarToggler.addEventListener('click', function() {
+      nav.classList.toggle('active');
+    });
+  });
+  
+ 
+// JavaScript to show footer when user scrolls to the bottom
+window.addEventListener("scroll", function() {
+    var footer = document.getElementById("site-footer");
+    var scrollPosition = window.innerHeight + window.pageYOffset;
+    var documentHeight = document.body.offsetHeight;
+  
+    // Show footer when user scrolls to the bottom
+    if (scrollPosition >= documentHeight) {
+      footer.style.display = "block";
+    } else {
+      footer.style.display = "none";
+    }
+  });
+  
