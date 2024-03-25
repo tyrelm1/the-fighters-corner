2c768612b05a607bc7683a691723429e9bbe82c9
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-03-25 11:48:21 +0000

    final commit before deployment

--- thefighterscorner/settings.py
+++ thefighterscorner/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['8000-tyrelm1-thefighterscorn-82mhgb8lggu.ws-eu110.gitpod.io','.herokuapp.com']
